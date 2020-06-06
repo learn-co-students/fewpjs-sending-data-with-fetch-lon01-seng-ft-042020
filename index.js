310639cbc7e17de7e65569aebaf76428d16dc738
gojoaromokudu <gojoaromokudu@gmail.com>
2020-06-06 21:13:58 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const body = document.querySelector('body')
+
+function submitData () {
+   return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+        })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+        const p = document.createElement('p');
+        p.innerHTML = object.id;
+        body.appendChild(p);
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+        const p = document.createElement('p');
+        p.innerHTML = error.message;
+        body.appendChild(p);
+    });
+
+}
